ddd4878ddd0915e253375479f99ab42e38d56c76
Me <vendramin.mitja@gmail.com>
2021-12-28 19:10:37 +0100

    200 ok on / url

--- api/src/main/java/si/fri/rso/stationcatalog/MainController.java
+++ api/src/main/java/si/fri/rso/stationcatalog/MainController.java
@@ -1,23 +1,25 @@
 package si.fri.rso.stationcatalog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import si.fri.rso.stationcatalog.models.entities.Station;
 import si.fri.rso.stationcatalog.services.StationService;
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/station") // This means URL's start with /demo (after Application path)
+@RestController // This means that this class is a Controller
+//@RequestMapping(path="/") // This means URL's start with /demo (after Application path)
 public class MainController {
 
     @Autowired
     private StationService stationService;
 
-    @PostMapping(path="/add") // Map ONLY POST Requests
+    @GetMapping(path={})
+    public ResponseEntity healthCheck(){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(path="/station/add") // Map ONLY POST Requests
     public @ResponseBody String addNewUser (@RequestParam String owner, @RequestParam double lat, @RequestParam double lon) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
@@ -31,7 +33,7 @@ public class MainController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path="/station/all")
     public @ResponseBody Iterable<Station> getAllUsers() {
         // This returns a JSON or XML with the users
         return stationService.getAllStations();
