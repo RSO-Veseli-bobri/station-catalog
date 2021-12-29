c443a99662523f94f585358c640b856107fcc276
Me <vendramin.mitja@gmail.com>
2021-12-29 23:01:34 +0100

    config server

--- api/src/main/java/si/fri/rso/stationcatalog/MainController.java
+++ api/src/main/java/si/fri/rso/stationcatalog/MainController.java
@@ -1,6 +1,9 @@
 package si.fri.rso.stationcatalog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +12,11 @@ import si.fri.rso.stationcatalog.services.StationService;
 
 @RestController // This means that this class is a Controller
 //@RequestMapping(path="/") // This means URL's start with /demo (after Application path)
+@RefreshScope
 public class MainController {
+
+    @Value("${allowCreation:true}")
+    private boolean canCreate;
 
     @Autowired
     private StationService stationService;
@@ -20,17 +27,21 @@ public class MainController {
     }
 
     @PostMapping(path="/station/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String owner, @RequestParam double lat, @RequestParam double lon) {
+    @ResponseBody
+    public ResponseEntity addNewUser (@RequestParam String owner, @RequestParam double lat, @RequestParam double lon) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
+        if(canCreate == false){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         Station n = new Station();
         n.setOwner(owner);
         n.setLat(lat);
         n.setLon(lon);
         n.setReserved(false);
         stationService.addStation(n);
-        return "Saved";
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping(path="/station/all")
